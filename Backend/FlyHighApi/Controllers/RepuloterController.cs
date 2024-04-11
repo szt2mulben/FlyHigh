ad9026160722023a920b9c2c6812a2d35bc7bd3d
szt2mulben <mulati.bence@gmail.com>
2024-04-11 11:18:10 +0200

    reptars backend

---
+++ Backend/FlyHighApi/Controllers/RepuloterController.cs
@@ -0,0 +1,32 @@
+﻿using FlyHighApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlyHighApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepuloterController : ControllerBase
+    {
+        private readonly flyhighdbContext _flyhighdbContext;
+        private readonly IConfiguration _configuration;
+
+        public RepuloterController(flyhighdbContext context, IConfiguration configuration)
+        {
+            _flyhighdbContext = context;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<JegyekModel>>> Getrepterek()
+        {
+            if (_flyhighdbContext.jegyek == null)
+            {
+                return NotFound();
+            }
+            return await _flyhighdbContext.jegyek.ToListAsync();
+        }
+
+
+    }
+}
---
+++ Backend/FlyHighApi/Models/Repuloter.cs
@@ -0,0 +1,6 @@
+﻿namespace FlyHighApi.Models
+{
+    public class Repuloter
+    {
+    }
+}
