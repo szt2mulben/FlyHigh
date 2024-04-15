15c73e42b4dbf775f6ddc15cad22476acf50f734
Péter Bácskai <bacskaipeter01@gmail.com>
2024-04-15 18:08:08 +0200

    repuloter httpput

--- Backend/FlyHighApi/Controllers/RepuloterController.cs
+++ Backend/FlyHighApi/Controllers/RepuloterController.cs
@@ -29,5 +29,68 @@ namespace FlyHighApi.Controllers
         }
 
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, RepuloterModels rep)
+        {
+            if (id != rep.Id)
+            {
+                return BadRequest();
+            }
+
+            _flyhighdbContext.Entry(rep).State = EntityState.Modified;
+
+            try
+            {
+                await _flyhighdbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!repExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deletet(int id)
+        {
+            if (_flyhighdbContext.repuloterek == null)
+            {
+                return NotFound();
+            }
+            var rep = await _flyhighdbContext.repuloterek.FindAsync(id);
+            if (rep == null)
+            {
+                return NotFound();
+            }
+
+            _flyhighdbContext.repuloterek.Remove(rep);
+            await _flyhighdbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpGet("search/{keresesiKriterium}")]
+        public async Task<ActionResult<IEnumerable<RepuloterModels>>> Search(string keresesiKriterium)
+        {
+            var rep = await _flyhighdbContext.repuloterek.Where(g => g.LastName.Contains(keresesiKriterium)).ToListAsync();
+            return rep;
+        }
+
+        private bool repExists(int id)
+        {
+            return (_flyhighdbContext.repuloterek?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+
     }
 }
