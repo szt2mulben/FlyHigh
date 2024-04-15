c5db30c0339036815a8fc6386d1d3287007f8b34
Péter Bácskai <bacskaipeter01@gmail.com>
2024-04-15 18:03:02 +0200

    search container

--- Frontend/flyhigh-web-beta1.0/src/components/Repulotarsasag/Repulotarsasag.vue
+++ Frontend/flyhigh-web-beta1.0/src/components/Repulotarsasag/Repulotarsasag.vue
@@ -163,6 +163,21 @@
   margin: 5px 0;
   font-weight: bold;
 }
+.search-container {
+  margin-bottom: 20px;
+}
+
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1000;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0,0,0,0.4);
+}
 
   </style>
     
